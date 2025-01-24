1e169363296a9cab558031b3853d0746a5700f9d
Shreya Patil <129930237+shreyapatil1972@users.noreply.github.com>
2025-01-24 21:58:29 +0530

    Operators and Expression

---
+++ Javascript-2.js
@@ -0,0 +1,91 @@
+// Arithmetic Operators
+// x = prompt("Enter the first number")
+// y = prompt("Enter the second number")
+// console.log("Addition:", x+y)
+// console.log("Substraction:", x-y)
+// console.log("Multiply:", x*y)
+// console.log("division:", x/y)
+// console.log("division:", x%y)
+
+// Comparison Operators
+// x = prompt("Enter the first number")
+// y = prompt("Enter the second number")
+// console.log(x>y)
+// console.log(x<y)
+// console.log(x==y)
+
+// logical Operator
+// mathscore = prompt("Enter the math score ")
+// sciscore = prompt("Enter the sci score ")
+// eligible = (mathscore>=90) && (sciscore>=85);
+// console.log("scholarship eligibility",eligible)
+
+// Assignment Operators
+// x=10
+// console.log("the x is:",x)
+// x+=5
+// console.log("the x after adding is:",x)
+// x*=3
+// console.log("the x after multiply is:",x)
+// x-=10
+// console.log("the x after substract:",x)
+// x/=2
+// console.log("the x after dividing is:",x)
+// x%=3
+// console.log("the x after modulus is:",x)
+
+// Operator Precedence
+// let cal1 = 10 + 3 * 5
+// let cal2 = (10 + 3) * 5
+// let cal3 = 20 / 2 + 3
+// let cal4 = 20 / (2 + 3)
+// console.log(cal1,cal2,cal3,cal4)
+
+// Combined Example
+// length = prompt("Enter the length of rectangle:")
+// width = prompt("Enter the width of rectangle:")
+// perimeter = 2*(length+width)
+// area = length*width
+// console.log("Is perimeter is greter than area", perimeter>area);
+
+// Complex Logical and Comparison Operation
+// age = prompt("Enter the age:")
+// iscitizen=prompt("Are you a Citizen?")==="true";
+// if(age>=18 && iscitizen){
+//     console.log("You are eligible for voting")
+// }else{
+//     console.log("You are not eligible for voting")
+// }
+
+
+//BMI Calculator
+// weight = prompt("Enter the weight in(kg):")
+// height = prompt("Enter the height in(m2):")
+// BMI = weight/height
+// if(BMI<18.5){
+//     console.log("Your Underweight")
+// }else if(18.5 <= BMI < 24.9){
+//     console.log("Your Normal")
+// }else if(25 <= BMI < 29.9){
+//     console.log("Your Overweight")
+// }else if(BMI >= 30){
+//     console.log("Your Obesity")
+// }
+
+
+// Even Or odd
+// num = prompt("Enter the number")
+// if(num%2==0){
+//     console.log("The number is even")
+// }else{
+//     console.log("The number is odd")
+// }
+
+// Shopping Discount
+// total = prompt("Enter the total amount:")
+// if(total>100){
+//     total -= total*0.1;
+// }else{
+//     console.log("no discount")
+// }
+// console.log("Discount total:",total)
